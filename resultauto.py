b1d6e942e31b237340a7d1fdaa0d609fd1f17def
GOKUL SUBRAMANIAN <96585296+GOKUL69@users.noreply.github.com>
2022-10-28 00:22:48 +0530

    Update resultauto.py

--- resultauto.py
+++ resultauto.py
@@ -1,28 +1,30 @@
 from selenium import webdriver 
-
 import http.client
 import time
 import os
-from selenium import webdriver
 import chromedriver_autoinstaller
-
-
-
 
 
 chromedriver_autoinstaller.install()
 driver=webdriver.Chrome()
-def whatsapp(result):
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
+def whatsapp():
   
-   message=result
-   payload = "token=zi2hk1ie8vevmc3m&to=+919360686061&body="+message+"&priority=1&referenceId="
+  
+   payload = "token=zi2hk1ie8vevmc3m&to=+919360686061&body=gokul&priority=1&referenceId="
    headers = { 'content-type': "application/x-www-form-urlencoded" }
    conn.request("POST", "/instance21482/messages/chat", payload, headers)
    
 
 def results():
  
- driver.maximize_window()
+ 
  driver.get("http://exam.pondiuni.edu.in/results/")
  const="19TH0426"
 
@@ -30,7 +32,7 @@ def results():
  driver.find_element_by_xpath('//*[@id="exam"]').send_keys("sixth")
  driver.find_element_by_xpath('//*[@id="print_app_form"]/span').click()
  time.sleep(1)
- whatsapp("hi")
+ 
   
  try:
   driver.switch_to.alert.accept()
